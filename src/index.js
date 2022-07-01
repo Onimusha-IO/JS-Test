462563f16ccad9c6449a4dc602f0dc2dc81cff21
Gustavo Bracho <61944183+Onimusha-IO@users.noreply.github.com>
2022-07-01 11:30:56 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<form>
+  <input type="text" id="usuario" placeholder=" "></input>
+  <label for="usuario">Usuario</label>
+</form>`;
